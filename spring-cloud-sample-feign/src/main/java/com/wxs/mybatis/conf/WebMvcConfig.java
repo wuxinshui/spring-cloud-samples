68f6a8740b2727884e6135e4035fbf23124e96ed
任富吉 <fuji.ren@bkjk.com>
2018-10-29 18:37:18 +0800

    fix

--- spring-cloud-sample-feign/src/main/java/com/wxs/mybatis/conf/WebMvcConfig.java
+++
@@ -1,9 +0,0 @@
-package com.wxs.mybatis.conf;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-@Configuration
-public class WebMvcConfig extends WebMvcConfigurerAdapter {
-
-}
