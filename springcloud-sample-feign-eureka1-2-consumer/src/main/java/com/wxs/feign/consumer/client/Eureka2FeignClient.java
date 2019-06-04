18cd350dae67bc95611d6585cfe8553595a3bc08
任富吉 <fuji.ren@bkjk.com>
2019-06-04 20:55:48 +0800

    add eureka1,eureka2,consumer

--- springcloud-sample-feign-eureka1-2-consumer/src/main/java/com/wxs/feign/consumer/client/Eureka2FeignClient.java
+++ springcloud-sample-feign-eureka1-2-consumer/src/main/java/com/wxs/feign/consumer/client/Eureka2FeignClient.java
@@ -3,7 +3,8 @@ package com.wxs.feign.consumer.client;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "feign-eureka2-provider")
+//@FeignClient(name = "feign-eureka2-provider",url = "http://localhost:8004")
+@FeignClient(name = "feign-eureka2-provider",url = "${provider2-server}")
 public interface Eureka2FeignClient {
 
     /**
