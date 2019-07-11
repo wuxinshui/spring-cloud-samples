4079f3d2a79f888868899c5684a975120d85fc41
任富吉 <fuji.ren@bkjk.com>
2019-07-11 16:03:49 +0800

    add hystrix

--- springcloud-sample-feign-provider-spi/src/main/java/com/wxs/feign/provider/api/CustomizeFeignApi.java
+++ springcloud-sample-feign-provider-spi/src/main/java/com/wxs/feign/provider/api/CustomizeFeignApi.java
@@ -1,5 +1,6 @@
 package com.wxs.feign.provider.api;
 
+import com.netflix.ribbon.proxy.annotation.CacheProvider;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
