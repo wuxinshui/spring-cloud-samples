0170ff975498ea6597c5ed7ef2c77df71f290f3f
任富吉 <fuji.ren@bkjk.com>
2019-07-19 13:34:00 +0800

    add

---
+++ springcloud-sample-feign-consumer/src/main/java/com/wxs/feign/consumer/client/PathVariableApi.java
@@ -0,0 +1,18 @@
+package com.wxs.feign.consumer.client;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Map;
+
+/**
+ * @Author: yoyo
+ * @Description:
+ * @Date: Created in 2019/7/19 11:58
+ */
+@FeignClient(name = "feign-provider")
+public interface PathVariableApi {
+    @GetMapping("/path/{value}")
+    public Map getRequestParam(@PathVariable("value") String value);
+}
---
+++ springcloud-sample-feign-consumer/src/main/java/com/wxs/feign/consumer/controller/PathVariableController.java
@@ -0,0 +1,36 @@
+package com.wxs.feign.consumer.controller;
+
+import com.wxs.feign.consumer.client.PathVariableApi;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+/**
+ * @Author: yoyo
+ * @Description: Feign : PathVariable参数传递测试
+ * @Date: Created in 2019/2/25 17:38
+ */
+@RestController
+@Slf4j
+public class PathVariableController {
+
+    @Autowired
+    private PathVariableApi pathVariableApi;
+
+
+    /**
+     * PathVariable
+     *
+     * @param value
+     * @return
+     */
+    @GetMapping("/path/{value}")
+    public Map getRequestParam(@PathVariable("value") String value) {
+        log.info("request-param value is {}", value);
+        return pathVariableApi.getRequestParam(value);
+    }
+}
